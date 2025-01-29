ed735923ad9e8b896a79b217685d2712368fddc9
Arjun-k-prasad <arjunkprasad17@gmail.com>
2025-01-29 14:55:05 +0530

    Update ExtendedEuclidean1.java

--- ExtendedEuclidean1.java
+++ ExtendedEuclidean1.java
@@ -1,45 +1,33 @@
 import java.util.Scanner;
 
-public class ExtendedEuclidean1 {
-    static int gcdExtended(int a, int b, int[] x, int[] y) {
-        if (a == 0) {
-            x[0] = 0;
-            y[0] = 1;
-            return b;
+class ExtendedEuclidean1{
+    public static void extendedEuclidean(int a, int b) {
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s\n", "q", "r1", "r2", "r", "t1", "t2", "t");
+        System.out.println("-----------------------------------------------------------");
+        int r1 = a, r2 = b;
+        int t1 = 1, t2 = 0;
+        int t = 0;
+        while (r2 > 0) {
+            int q = r1 / r2;
+            int r = r1 % r2;
+            t = t1 - q * t2;
+            System.out.printf("%-10d %-10d %-10d %-10d %-10d %-10d\n", q, r1, r2, r, t1, t2);
+            r1 = r2;
+            r2 = r;
+            t1 = t2;
+            t2 = t;
         }
-
-        int[] x1 = {0}, y1 = {0};
-        int gcd = gcdExtended(b % a, a, x1, y1);
-
-        x[0] = y1[0] - (b / a) * x1[0];
-        y[0] = x1[0];
-
-        return gcd;
-    }
-
-    static int modInverse(int a, int m) {
-        int[] x = {0}, y = {0};
-        int gcd = gcdExtended(a, m, x, y);
-        if (gcd != 1) {
-            throw new IllegalArgumentException("Inverse does not exist");
-        }
-        return (x[0] % m + m) % m;
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("GCD: " + r1);
+        System.out.println("Multiplicative Inverse: " + (t1 < 0 ? t1 + b : t1));
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        int a = scanner.nextInt();
-        System.out.print("Enter modulus: ");
-        int m = scanner.nextInt();
-        
-        try {
-            int inverse = modInverse(a, m);
-            System.out.println("Modular Inverse: " + inverse);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        
-        scanner.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two integers (a and b) to compute the GCD and modular inverse:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        extendedEuclidean(a, b);
+        sc.close();
     }
 }
